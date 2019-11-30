37878b716dfcbf71ecbcb7b3afc25b9ca15b49c2
henlovelybird <voxylophone@gmail.com>
2019-11-30 18:30:18 +0100

    added onClick => ArtistPage

--- src/Components/ArtistPage.jsx
+++ src/Components/ArtistPage.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import {Container} from 'reactstrap';
 
 class ArtistPage extends React.Component {
     render() {
         let artist = this.props.artist;
+        let album = this.props.album;
           return (
+            <Container className="float-left mt-5 mb-0">
                <div className="col-md-3">
                  <img style={{width: "100%"}} 
                   src={artist.picture} 
-                  alt={"artist album cover for" + artist.cover} />
+                  alt={"artist album cover for" + album.cover} />
                   <span>{artist.name}</span> 
+                  <span>{album.title}</span>
               </div>
+            </Container>
               );
             }
   }
--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {Container} from 'reactstrap';
 import ArtistPage from './ArtistPage';
-import { Row } from 'react-bootstrap';
-let artistNames =  ['eminem', 'Radiohead']; //can also be in line 14
+import {Link} from 'react-router-dom';
+
 
 class HomePage extends React.Component {
     state = {
@@ -11,7 +11,8 @@ class HomePage extends React.Component {
     }
 
     componentDidMount = async () => {
-
+        let artistNames =  ['tupac', 'mos def']; //can also be in line 4
+        // i had this in before but i took it out bc i wanted to be able to search for any artist i wanted. 
 
         artistNames.forEach(async artist => {
             let response = await fetch("http://www.omdbapi.com/?apikey=24ad60e9&s=" + artist + "&limit=6",{
@@ -25,22 +26,19 @@ class HomePage extends React.Component {
             console.log(artistInfo.data)
             this.setState({
                 artist: artistInfo.data
-            });
-        }
+                });
+            }
         )
     }
-        
-     
+    
     render() {
-        let artist = this.state.artist;
+        // let artist = this.state.artist; 
         return (
             <Container flex>
-                <Row>
-                    <div>{this.props.search}</div>
-                </Row>
             {this.state.artist.map((artist, index) =>
-                <ArtistPage artists={artist.items} key={index} artist={artist.name} />)}
-        </Container>
+                <ArtistPage artists={artist.items} key={index} artist={artist.name} onClick={() => <Link to="/ArtistPage">{artist.picture}</Link>}/>)} 
+                {/* or can also be <Link to="/ArtistPage"/>? */}
+            </Container>
     );
 }
 }
