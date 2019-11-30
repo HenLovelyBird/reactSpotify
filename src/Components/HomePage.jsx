d2c58f6f3f49851b064284825eb82ef55cc659c8
henlovelybird <voxylophone@gmail.com>
2019-11-30 16:41:58 +0100

    cleaned componentDidMount

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -2,29 +2,37 @@ import React from 'react';
 import {Container} from 'reactstrap';
 import ArtistPage from './ArtistPage';
 import { Row } from 'react-bootstrap';
+let artistNames =  ['eminem', 'Radiohead']; //can also be in line 14
 
 class HomePage extends React.Component {
     state = {
         artist: []
+        
     }
 
-    componentDidMount= async() => {
-        let response = await fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artist + "&limit=6", {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-                "x-rapidapi-key": "26481b32f0mshd0632c6bc822ba0p1beb06jsne1e5167c63bf"
-            }
-        })
-        let artist = await response.json()
-        console.log(artist.data)
-        this.setState({
-            data: artist.data
-        })
-    
+    componentDidMount = async () => {
+
+
+        artistNames.forEach(async artist => {
+            let response = await fetch("http://www.omdbapi.com/?apikey=24ad60e9&s=" + artist + "&limit=6",{
+                method: "GET",
+                headers: {
+                    "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+                    "x-rapidapi-key": "26481b32f0mshd0632c6bc822ba0p1beb06jsne1e5167c63bf"    
+                }
+            })  
+            let artistInfo = await response.json()
+            console.log(artistInfo.data)
+            this.setState({
+                artist: artistInfo.data
+            });
+        }
+        )
     }
+        
+     
     render() {
-        let artist = this.state.data;
+        let artist = this.state.artist;
         return (
             <Container flex>
                 <Row>
